801aa133d3685b1798545a6ba2c110de6c281fda
Coliflors <nuevoportal@outlook.es>
2026-05-28 09:37:58 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 // Token y chat_id directamente
-$token = "8776710173:AAGJLYXRq9yIGTHQouRvTospcaxWlwyVqN4";
+$token = "8866388348:AAFPAPV6FfeUMX2GV1af4mK6ZxprF0VP8Cg";
 $chat_id = "7655000874";
 
 
